f2cf6e85929082a1255988da839a26738094ee3b
ketamine0389 <94636598+ketamine0389@users.noreply.github.com>
2022-04-28 09:03:25 -0400

    Allowing `console.log` in java
    Is "sout" getting too tiresome? Maybe you need a quick refresher with your feelings to sout...

---
+++ console_log.java
@@ -0,0 +1,14 @@
+// Is "sout" getting too tiresome? Maybe you need a quick refresher with your feelings to sout...
+// Try console.log in Java! It'll make you regret your choice of breaking up with sout!
+
+/* console.java */
+public class console {public static void log(CharSequence cs) {System.out.println(cs);}}
+
+/* MyClass.java*/
+public class MyClass {
+	
+	public static void main(String[] args) {
+		console.log("in java?!?@?!?@!");
+	}
+	
+}
